99e5ad02a6a315bd8b8d50624ea2cf131da3dfea
Pascal Cremer <b00gizm@gmail.com>
2014-12-05 22:43:30 +0100

    Initial support for basic callbacks

---
+++ test/fixtures.js
@@ -0,0 +1,137 @@
+var util      = require('util');
+var multiline = require('multiline');
+var lodash    = require('lodash');
+
+module.exports.genericRequest = function(methodName, params) {
+
+  var xml = multiline(function() {/*
+    <?xml version="1.0"?>
+    <methodCall>
+      <methodName>%s</methodName>
+      <params>%s</params>
+    </methodCall>
+  */});
+
+  if (lodash.isArray(params)) {
+    xmlParams = params.map(function(pair) {
+      if (lodash.isArray(pair)) {
+        return '<param><value><' + pair[0] + '>' + pair[1]  + '</' + pair[0] + '></value></param>';
+      }
+
+      if (!pair) {
+        return;
+      }
+
+      return '<param>' + pair + '</param>';
+    });
+
+    return util.format(xml, methodName, xmlParams);
+  }
+
+  return util.format(xml, methodName, params || '');
+};
+
+module.exports.genericResponse = function(params) {
+  var xml = multiline(function() {/*
+    <?xml version="1.0"?>
+    <methodResponse>
+      <params>%s</params>
+    </methodResponse>
+    */});
+
+  if (lodash.isArray(params)) {
+    xmlParams = params.map(function(pair) {
+      if (lodash.isArray(pair)) {
+        return '<param><value><' + pair[0] + '>' + pair[1]  + '</' + pair[0] + '></value></param>';
+      }
+
+      if (!pair) {
+        return;
+      }
+
+      return '<param>' + pair + '</param>';
+    });
+
+    return util.format(xml, xmlParams);
+  }
+
+  return util.format(xml, params || '');
+};
+
+module.exports.newPostRequest = function(title, body, categories, keywords) {
+  var xml = multiline(function() {/*
+    <?xml version="1.0"?>
+    <methodCall>
+      <methodName>metaWeblog.newPost</methodName>
+      <params>
+        <param>
+          <value>
+            <int>4711</int>
+          </value>
+        </param>
+        <param>
+          <value>
+            <string>johnedoe</string>
+          </value>
+        </param>
+        <param>
+          <value>
+            <string>s3cr3t</string>
+          </value>
+        </param>
+        <param>
+          <value>
+            <struct>
+              <member>
+                <name>title</name>
+                <value>
+                  <string>%s</string>
+                </value>
+              </member>
+              <member>
+                <name>description</name>
+                <value>
+                  <string>
+                    <![CDATA[%s]]>
+                  </string>
+                </value>
+              </member>
+              <member>
+                <name>categories</name>
+                <value>
+                  <array>
+                    <data>%s</data>
+                  </array>
+                </value>
+              </member>
+              <member>
+                <name>mt_keywords</name>
+                <value>
+                  <array>
+                    <data>%s</data>
+                  </array>
+                </value>
+              </member>
+            </struct>
+          </value>
+        </param>
+      </params>
+    </methodCall>
+    */});
+
+  categoriesXML = (categories || [])
+    .map(function(category) {
+      return '<value>' + category + '</value>';
+    })
+    .join("\n")
+  ;
+
+  keywordsXML = (keywords || [])
+    .map(function(category) {
+      return '<value>' + category + '</value>';
+    })
+    .join("\n")
+  ;
+
+  return util.format(xml, title, body, categoriesXML, keywordsXML);
+};
--- test/webhook-test.js
+++ test/webhook-test.js
@@ -1,9 +1,10 @@
-var chai    = require('chai');
-var express = require('express');
-var request = require('supertest');
-var should  = chai.should();
+var chai     = require('chai');
+var express  = require('express');
+var request  = require('supertest');
+var fixtures = require('./fixtures');
+var should   = chai.should();
 
-var webhook = require('../lib/webhook');
+var webhook  = require('../lib/webhook');
 
 describe('webhook', function() {
 
@@ -13,7 +14,7 @@ describe('webhook', function() {
   });
 
   describe('middleware', function() {
-    before(function() {
+    beforeEach(function() {
       var app = express();
 
       app.use(webhook());
@@ -33,6 +34,157 @@ describe('webhook', function() {
         .expect(200)
         .expect('Hello World!', done)
       ;
+    });
+
+    it('should expose /xmlrpc.php endpoint', function(done) {
+      var agent = request.agent(this.app);
+
+      agent
+        .get('/xmlrpc.php')
+        .expect(404)
+        .expect('content-type', /text\/xml/, done)
+      ;
+    });
+
+    it('should expose /wp-admin/profile.php endpoint', function(done) {
+      var agent = request.agent(this.app);
+
+      agent
+        .get('/wp-admin/profile.php')
+        .expect(200, done)
+      ;
+    });
+
+    it('should expose /wp-admin/options-discussion.php endpoint', function(done) {
+      var agent = request.agent(this.app);
+
+      agent
+        .get('/wp-admin/options-discussion.php')
+        .expect(200, done)
+      ;
+    });
+
+    it('should expose /wp-admin/options-general.php endpoint', function(done) {
+      var agent = request.agent(this.app);
+
+      agent
+        .get('/wp-admin/options-general.php')
+        .expect(200, done)
+      ;
+    });
+
+    it('should return XML response for "mt.supportedMethods" XML-RPC request', function(done) {
+      var agent = request.agent(this.app);
+
+      var xmlPayload = fixtures.genericRequest('mt.supportedMethods');
+
+      agent
+        .post('/xmlrpc.php')
+        .set({
+          'Content-Type'   : 'text/xml',
+          'Accept'         : 'text/xml',
+          'Accept-Charset' : 'UTF8'
+        })
+        .send(xmlPayload)
+        .expect(200)
+        .expect('content-type', /text\/xml/, done)
+      ;
+    });
+
+    it('should return XML response for "metaWeblog.getRecentPosts" XML-RPC request', function(done) {
+      var agent = request.agent(this.app);
+
+      var xmlPayload = fixtures.genericRequest('metaWeblog.getRecentPosts');
+
+      agent
+        .post('/xmlrpc.php')
+        .set({
+          'Content-Type'   : 'text/xml',
+          'Accept'         : 'text/xml',
+          'Accept-Charset' : 'UTF8'
+        })
+        .send(xmlPayload)
+        .expect(200)
+        .expect('content-type', /text\/xml/, done)
+      ;
+    });
+
+    describe('IFTTT request', function() {
+      // describe('with default middleware');
+
+      describe('with middleware callback', function() {
+        beforeEach(function() {
+          this.app = express();
+        });
+
+        it('should call the callback', function(done) {
+          this.app.use(webhook(function(json, done) {
+            json.should.eql({
+              username    : 'johnedoe',
+              password    : 's3cr3t',
+              title       : 'A title',
+              description : 'A body',
+              categories  : [ 'one', 'two', 'three' ],
+              tags        : [ 'four', 'five', 'six' ]
+            });
+
+            done();
+          }));
+
+          var agent = request.agent(this.app);
+
+          var xmlPayload = fixtures.newPostRequest('A title', 'A body', ['one', 'two', 'three'], ['four', 'five', 'six']);
+
+          agent
+            .post('/xmlrpc.php')
+            .set({
+              'Content-Type'   : 'text/xml',
+              'Accept'         : 'text/xml',
+              'Accept-Charset' : 'UTF8'
+            })
+            .send(xmlPayload)
+            .expect(200)
+            .expect('content-type', /text\/xml/, done)
+          ;
+        });
+
+        it('should try to parse JSON object from body', function(done) {
+          this.app.use(webhook(function(json, done) {
+            json.should.eql({
+              username    : 'johnedoe',
+              password    : 's3cr3t',
+              title       : 'A title',
+              description : { foo: 123, bar: 234 },
+              categories  : [ 'one', 'two', 'three' ],
+              tags        : [ 'four', 'five', 'six' ]
+            });
+
+            done();
+          }));
+
+          var agent = request.agent(this.app);
+
+          var xmlPayload = fixtures.newPostRequest('A title', '{"foo": 123, "bar": 234}', ['one', 'two', 'three'], ['four', 'five', 'six']);
+
+          agent
+            .post('/xmlrpc.php')
+            .set({
+              'Content-Type'   : 'text/xml',
+              'Accept'         : 'text/xml',
+              'Accept-Charset' : 'UTF8'
+            })
+            .send(xmlPayload)
+            .expect(200)
+            .expect('content-type', /text\/xml/, done)
+          ;
+        });
+      });
+
+      // describe('with middleware auth');
+      //
+      // describe('with middleware categories');
+      //
+      // describe('with middleware auth and categories');
     });
   });
 
